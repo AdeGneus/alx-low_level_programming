73281fecf442235f43ee7316ee4bb94d55f02800
Wasiu Bakare <wasiubakare@gmail.com>
2022-03-15 16:54:10 +0100

    alphabets only

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,8 +5,10 @@
  * print_alphabet - writes the letters of the alphabet to stdout
  * print_alphabet_x10 - writes the alphabet 10 times
  * _islower - check whether the character is lowercase or not.
+ * _isalpha - check for alphabetic character
  */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+int _isalpha(int c);
