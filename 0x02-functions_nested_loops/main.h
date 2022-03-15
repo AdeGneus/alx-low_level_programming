827924e69cd1a2a0cd082dcca282e42cbe7089fe
Wasiu Bakare <wasiubakare@gmail.com>
2022-03-15 17:40:21 +0100

    print sign of int

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,9 +6,11 @@
  * print_alphabet_x10 - writes the alphabet 10 times
  * _islower - check whether the character is lowercase or not.
  * _isalpha - check for alphabetic character
+ * print_sign - prints the sign of a number
  */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
+int print_sign(int n);
