10281b079c79fbc8dfceedc1ea035ac5fe2c11ef
Wasiu Bakare <wasiubakare@gmail.com>
2022-03-17 21:46:56 +0100

    multiplication

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,7 +4,9 @@
  * _putchar - writes the character c to stdout
  * _isupper - checks if char is uppercase
  * _isdigit - checkd if c is a digit
+ * mul - multiplies two numbers and return the result
  */
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
