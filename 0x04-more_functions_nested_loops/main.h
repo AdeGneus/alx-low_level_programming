2f307e3c422b7c1e3633efbfa697ee868e5115ef
Wasiu Bakare <wasiubakare@gmail.com>
2022-03-17 21:53:30 +0100

    print 0-9

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,8 +5,10 @@
  * _isupper - checks if char is uppercase
  * _isdigit - checkd if c is a digit
  * mul - multiplies two numbers and return the result
+ * print_numbers - print numbers from 0 - 9 to stdout
  */
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
