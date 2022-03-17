b679ae0339129a4b3a284b9b36b42581a30379b0
Wasiu Bakare <wasiubakare@gmail.com>
2022-03-17 22:35:20 +0100

    print 0 -14

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,6 +7,7 @@
  * mul - multiplies two numbers and return the result
  * print_numbers - print numbers from 0 - 9 to stdout
  * print_most_numbers - print 0-9 except 2 & 4
+ * more_numbers - print 1 - 14 to stdout 10 times
  */
 int _putchar(char c);
 int _isupper(int c);
@@ -14,3 +15,4 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
