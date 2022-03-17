5e525fee30f401397edece08e590efb06caea2fa
Wasiu Bakare <wasiubakare@gmail.com>
2022-03-17 22:21:58 +0100

    skipped 2 & 4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,9 +6,11 @@
  * _isdigit - checkd if c is a digit
  * mul - multiplies two numbers and return the result
  * print_numbers - print numbers from 0 - 9 to stdout
+ * print_most_numbers - print 0-9 except 2 & 4
  */
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
