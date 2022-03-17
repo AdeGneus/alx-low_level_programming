cb92bb612bef6f5d852487f5611ea39072d84396
Wasiu Bakare <wasiubakare@gmail.com>
2022-03-17 21:36:11 +0100

    checked if digit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,6 +3,8 @@
 /**
  * _putchar - writes the character c to stdout
  * _isupper - checks if char is uppercase
+ * _isdigit - checkd if c is a digit
  */
 int _putchar(char c);
 int _isupper(int c);
+int _isdigit(int c);
