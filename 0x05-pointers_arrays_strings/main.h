89f35079267da6eff0f5b621855d5ae7f2dec811
Wasiu Bakare <wasiubakare@gmail.com>
2022-03-21 15:20:08 +0100

    length of a string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,7 +4,9 @@
  * _putchar - writes the character c to stdout
  * reset_to_98 - updates the value of a pointer to 98
  * swap_int - swaps the values of two int
+ * _strlen - returns the length of a string
  */
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
