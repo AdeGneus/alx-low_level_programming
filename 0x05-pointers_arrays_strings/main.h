d1cb721a45cb8607f81029fa9f5bb1bdeefbb161
Wasiu Bakare <wasiubakare@gmail.com>
2022-03-21 16:36:48 +0100

    print second half of a string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,6 +9,7 @@
  * print_rev - prints a string in reverse
  * rev_string - reverses a string
  * puts2 - prints every other character of a string
+ * puts_half - prints second half of a string
  */
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -18,3 +19,4 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
