6ec3aae40d235bddc84b39da2a7b2db3feb37946
Wasiu Bakare <wasiubakare@gmail.com>
2022-03-21 15:01:55 +0100

    reset to 98

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * reset_to_98 - updates the value of a pointer to 98
+ */
+int _putchar(char c);
+void reset_to_98(int *n);
