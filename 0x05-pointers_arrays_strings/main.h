0e7a31dca3011e378f07ba245fc4d0d308da29c5
Wasiu Bakare <wasiubakare@gmail.com>
2022-03-21 15:28:31 +0100

    print a string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,8 +5,10 @@
  * reset_to_98 - updates the value of a pointer to 98
  * swap_int - swaps the values of two int
  * _strlen - returns the length of a string
+ * _puts - prints a string followed by a new line to stdout
  */
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
