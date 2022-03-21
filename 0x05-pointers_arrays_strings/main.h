712a8e8a778dd84ae05ff9d767a6090bbec45434
Wasiu Bakare <wasiubakare@gmail.com>
2022-03-21 15:09:44 +0100

    swap values of two int

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,8 @@
 /**
  * _putchar - writes the character c to stdout
  * reset_to_98 - updates the value of a pointer to 98
+ * swap_int - swaps the values of two int
  */
 int _putchar(char c);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
