719eee970cce65c76d37504b844244f7a6dbcafc
Wasiu Bakare <wasiubakare@gmail.com>
2022-03-24 14:21:53 +0100

    compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: positive, negative or zero
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0;
+	int b = 0;
+	int c;
+
+	while (s1[a] != '\0')
+		a++;
+
+	while (s2[b] != '\0')
+		b++;
+
+	if (a < b)
+		c = -15;
+	else if (a > b)
+		c = 15;
+	else
+		c = 0;
+
+	return (c);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,8 +5,10 @@
  * *_strcat - concatenates two strings
  * *_strncat - appends n bytes of src to dest
  * *_strncpy - copies a string
+ * -strcmp - compares two strings
  */
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
