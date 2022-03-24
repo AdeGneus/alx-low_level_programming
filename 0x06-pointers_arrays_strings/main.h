28acdd428a428c531472b5d5b17efa680c6ac3f7
Wasiu Bakare <wasiubakare@gmail.com>
2022-03-24 10:09:09 +0100

    concatenate n bytes of char to a string

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,8 @@
 /**
  * _putchar - writes the character c to stdout
  * *_strcat - concatenates two strings
+ * *_strncat - appends n bytes of src to dest
  */
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
