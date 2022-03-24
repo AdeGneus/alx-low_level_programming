9882dffb75bfac08e3675a5d18474fc7cb05d9ea
Wasiu Bakare <wasiubakare@gmail.com>
2022-03-24 18:14:41 +0100

    fixed 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,17 +10,15 @@ char *leet(char *str)
 	int a, b;
 	char *replaced;
 
-	a = 0;
 	replaced = "aAeEoOtTlL4433007711";
 
-	while (str[a] != '\0')
+	for (a = 0; str[a] != '\0'; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
 			if (str[a] == replaced[b]
 				str[a] = replaced[b + 10];
 		}
-		a++;
 	}
 	return (str);
 }
