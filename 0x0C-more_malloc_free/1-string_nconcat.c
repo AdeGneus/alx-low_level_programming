08d564b30676286c70dfbfb7aedebc0bc1e80c51
Wasiu Bakare <wasiubakare@gmail.com>
2022-04-07 13:23:55 +0100

    fixed edge cases

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,34 +16,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	size1 = 0;
 	size2 = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
 	while (s1[size1] != '\0')
 		size1++;
 	while (s2[size2] != '\0')
 		size2++;
+	if (size2 > n)
+		size2 = n;
 
 	dest = malloc((size1 + size2 + 1) * sizeof(char));
 
 	if (dest == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; i < size1; i++)
 		dest[i] = s1[i];
 
-	if (n >= size2)
-	{
-		for (i = 0; s2[i] != '\0'; i++)
-			dest[size1 + i] = s2[i];
-	}
-	else
-	{
-		for (i = 0; i < n; i++)
-			dest[size1 + 1] = s2[i];
-	}
+	for (i = 0; i < size2; i++)
+		dest[size1 + i] = s2[i];
+
 	dest[size1 + size2] = '\0';
 	return (dest);
 }
