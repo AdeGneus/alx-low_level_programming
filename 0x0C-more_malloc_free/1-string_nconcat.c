6a7775613823725cb3e7ad9c6eb0cc1efb565e0b
Wasiu Bakare <wasiubakare@gmail.com>
2022-04-07 13:52:24 +0100

    fixed compile error

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		size2 = n;
 
 	dest = malloc((size1 + size2 + 1) * sizeof(char));
-	if (size == NULL)
+	if (dest == NULL)
 		return (NULL);
 
 	for (i = 0; i < size1; i++)
