8c4f8911394018d1f73328567b19b14b9a8229a7
Wasiu Bakare <wasiubakare@gmail.com>
2022-04-11 14:14:40 +0100

    fixed code output

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
 #ifndef _3_FUNCTION_LIKE_MACRO_H_
 #define _3_FUNCTION_LIKE_MACRO_H_
 
-#define ABS(X) (((X) < 0) ? (-X) : (X))
+#define ABS(X) (((X) < 0) ? ((X) * -1) : (X))
 #endif /* 3_FUCTION_LIKE_MACRO */
