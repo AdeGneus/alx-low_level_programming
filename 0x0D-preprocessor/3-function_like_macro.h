c95e340380f8ee4338af84a889cd34918b12b9c7
Wasiu Bakare <wasiubakare@gmail.com>
2022-04-11 14:10:46 +0100

    fixed macro expansion

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,5 @@
-#ifndef ABS
-#define ABS(X) ((X < 0) ? (-X) : (X))
-#endif /* ABS */
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
+
+#define ABS(X) (((X) < 0) ? (-X) : (X))
+#endif /* 3_FUCTION_LIKE_MACRO */
