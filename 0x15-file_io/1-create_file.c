cb131055eb7e7882c66b8e02a1631e0274535923
Wasiu Bakare <wasiubakare@gmail.com>
2022-05-02 20:30:01 +0100

    fixed error in 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	length = 0;
 	if (text_content != NULL)
 	{
-		while (text_content[length] != NULL)
+		while (text_content[length])
 			length++;
 	}
 
