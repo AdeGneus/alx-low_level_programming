6ef924b8667f26e3cd15857dd6b3e26f7052223e
Wasiu Bakare <wasiubakare@gmail.com>
2022-06-13 23:55:22 +0100

    qwin

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/win.so https://github.com/AdeGneus/alx-low_level_programming/raw/main/0x18-dynamic_libaries/win.so
+wget -q -O /tmp/win.so https://github.com/AdeGneus/alx-low_level_programming/blob/main/0x18-dynamic_libraries/win.so
 export LD_PRELOAD=/tmp/win.so
